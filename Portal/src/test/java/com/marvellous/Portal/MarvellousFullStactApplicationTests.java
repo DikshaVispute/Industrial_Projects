bb3efe79f26d118fa9dcde6f5f4b59cefab2a0fc
DikshaVispute <dikshavispute809@gmail.com>
2025-12-07 13:00:38 +0530

    Add project (Portal)

---
+++ Portal/src/test/java/com/marvellous/Portal/MarvellousFullStactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvellous.Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvellousFullStactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
